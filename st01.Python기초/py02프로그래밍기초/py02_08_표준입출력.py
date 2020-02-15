efc3896d7f2e9bf674ea49ec40f820875b7f53c3
sejoong <sjydevil@gmail.com>
2020-02-16 06:43:08 +0900

    FDSAFASD

--- st01.Python기초/py02프로그래밍기초/py02_08_표준입출력.py
+++ st01.Python기초/py02프로그래밍기초/py02_08_표준입출력.py
@@ -1,3 +1,4 @@
 x = input("입력하세요 >> ")
 print("입력: ", x)
 print("타입: ", type(x))
+
--- st01.Python기초/py03변수/py03_07_변수가저장하는것.py
+++ st01.Python기초/py03변수/py03_07_변수가저장하는것.py
@@ -3,12 +3,32 @@
 # 파이썬에서는 모든 것이 객체로 처리된다.
 
 a = 3
-print("reference address >> ", id(a))
+b = a
+print("value >> ", a, b)
+print("reference address >> ", id(a), id(b))
+print("실제값 일치 여부 확인 >> ", a == b)  # 일치 연산자 : 실제값 일치 여부 확인
+print("주소값 일체 여부 확인 >> ", id(a) == id(b))  # 일치 연산자 : 주소값 일체 여부 확인
+print("is 연산자 >> ", a is b)  # is 연산자
+
+print()
+print()
 
 
 a = 1000
-print("reference address >> ", id(a))
+b = a
+print("value >> ", a, b)
+print("reference address >> ", id(a), id(b))
+print("실제값 일치 여부 확인 >> ", a == b)  # 일치 연산자 : 실제값 일치 여부 확인
+print("주소값 일체 여부 확인 >> ", id(a) == id(b))  # 일치 연산자 : 주소값 일체 여부 확인
+print("is 연산자 >> ", a is b)  # is 연산자
 
+print()
+print()
 
 a = 1000.01
-print("reference address >> ", id(a))
+b = a
+print("value >> ", a, b)
+print("reference address >> ", id(a), id(b))
+print("실제값 일치 여부 확인 >> ", a == b)  # 일치 연산자 : 실제값 일치 여부 확인
+print("주소값 일체 여부 확인 >> ", id(a) == id(b))  # 일치 연산자 : 주소값 일체 여부 확인
+print("is 연산자 >> ", a is b)  # is 연산자
