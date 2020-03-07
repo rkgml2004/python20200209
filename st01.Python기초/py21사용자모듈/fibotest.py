0fa10a658c1972d21178fada50b2d8a4798a9efd
sejoong <sjydevil@gmail.com>
2020-03-08 06:34:05 +0900

    fdasfas

---
+++ st01.Python기초/py21사용자모듈/fibotest.py
@@ -0,0 +1,8 @@
+# import 방식
+# 모듈 import
+# 함수 import
+
+# import 모듈이름
+# from 모듈이름 import 모듈함수
+
+# 이 모듈이 단독으로 사용되면 main()를 호출하라.
---
+++ st01.Python기초/py23표준모듈/py23_07_random모듈.py
@@ -0,0 +1,27 @@
+#################################
+# random 모듈의 사용법을 익힌다.
+#
+# random.random()	0.0 <= x < 1.0 사이의 float을 리턴
+# random.uniform(10, 20)	지정한 범위 사이의 float을 리턴
+# random.randrange(min, max)	min부터 max 사이의 int을 리턴
+# random.choice(리스트)	리스트의 요소들을 랜덤하게 한 개 선택
+# random.shuffle(리스트)	리스트의 요소들을 랜덤하게 섞기
+# random.sample(리스트)	리스트의 요소 중에 k개
+#################################
+
+
+# 모듈을 읽어 들입니다.
+
+# random(): 0.0 <= x < 1.0 사이의 float을 리턴합니다.
+
+# uniform(min, max): 지정한 범위 사이의 float을 리턴합니다.
+
+# randrange(): 지정한 범위의 int를 리턴합니다.
+# - randrange(max): 0부터 max 사이의 int을 리턴합니다.
+# - randrange(min, max): min부터 max 사이의 int을 리턴합니다.
+
+# choice(list): 리스트 내부에 있는 요소를 랜덤하게 선택합니다.
+
+# shuffle(list): 리스트의 요소들을 랜덤하게 섞습니다.
+
+# sample(list, k=<숫자>): 리스트의 요소 중에 k개를 뽑습니다.
